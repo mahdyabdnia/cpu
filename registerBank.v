e558b6b443532f834124d01d56d560bf6c7143f6
mahdyabdnia <44471104+mahdyabdnia@users.noreply.github.com>
2018-12-02 19:48:10 +0330

    Add files via upload

--- registerBank.v
+++ registerBank.v
@@ -1,13 +1,15 @@
-module register(Ad_a,Ad_b,Ad_c,clk,data_wr,wr_acc);
-input [0:4] Ad_a,Ad_b,Ad_c;
+module register(data_a,data_b,Ad_a,Ad_b,Ad_c,clk,data_wr,wr_acc,);
+ output [0:63]data_a,data_b;
+input [0:4]Ad_a,Ad_b,Ad_c;
 input clk;
 input [0:63]data_wr;
 input wr_acc;
-output reg [0:63] data_a,data_b;
 reg [63:0] mem [0:31];
 
-assign  #2 data_a=mem[Ad_a];
-assign  #2 data_b=mem[Ad_b];
+
+
+assign  #2 data_a = mem[Ad_a];
+assign  #2 data_b = mem[Ad_b];
 
 always @(negedge clk)
 begin
