930c7b5947116d0ec763d728b2ba0cd71ec0590d
mahdyabdnia <44471104+mahdyabdnia@users.noreply.github.com>
2018-12-16 10:38:44 +0330

    Add files via upload

--- Adder.v
+++ Adder.v
@@ -1,11 +1,12 @@
-module Adder(in,imm,out)
+module Adder(clk,in,imm,out)
+input clk;
 input [0:63] in;
 input [0:63]imm;
 output reg[0:63] out;
 
 
 assign imm=64'b100
-always @(in,imm)
+always @(negedge clk)
 begin 
 
 out=in + imm;
